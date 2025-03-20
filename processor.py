30a41bcb8fdd78cf43c0b7a30d44b3f110a85d93
Andres Molina <andres.molina@altris.se>
2025-03-20 13:16:51 +0100

    Fixed legend in single viscosity plot

--- processor.py
+++ processor.py
@@ -1,5 +1,6 @@
 from data_import import *
 from plotting import *
+import re
 
 
 class DataProcessor:
@@ -38,51 +39,19 @@ class DataProcessor:
         # Full path for output file
         full_output_path = os.path.join(self.output_directory, fig_name)
 
+        # Name of the dataset
+        name = re.split('[-_]', fig_name)[0]
+
         # Plot data
         plot_viscosity_data(
             df=df,
             fig_name=fig_name,
             export_path=self.output_directory,
-            sweep_types=sweep_type
+            sweep_types=sweep_type,
+            datasets=name
         )
 
         return df, fig_name, full_output_path
-
-    def process_diff_viscosity_single(self, file_path, sweep_type):
-        """
-                Process a single differential viscosity data file and generate a plot.
-
-                Parameters:
-                file_path (str): Path to the viscosity data file.
-                sweep_type (str or list): Type of sweep (e.g., 'up', 'down', or ['up', 'down']).
-
-                Returns:
-                tuple: DataFrame containing processed data, filename of the generated plot, full path of the output file.
-                """
-        # Load data
-        df = load_viscosity_stress_data(file_path)
-
-        # Generate filename
-        fig_name = os.path.splitext(os.path.basename(file_path))[0]
-        if isinstance(sweep_type, list):
-            fig_name += "-BOTH.png"
-        else:
-            fig_name += f"-{sweep_type}.png"
-
-        # Full path for output file
-        full_output_path = os.path.join(self.output_directory, fig_name)
-
-        # Plot data
-        plot_diff_viscosity_data(
-            df=df,
-            fig_name=fig_name,
-            export_path=self.output_directory,
-            sweep_types=sweep_type
-        )
-
-        return df, fig_name, full_output_path
-
-
 
     def process_viscosity_multiple(self, file_paths, sweep_type):
         """
@@ -127,6 +96,41 @@ class DataProcessor:
         )
 
         return dataframes, dataset_names, fig_name, full_output_path
+
+    def process_diff_viscosity_single(self, file_path, sweep_type):
+        """
+                Process a single differential viscosity data file and generate a plot.
+
+                Parameters:
+                file_path (str): Path to the viscosity data file.
+                sweep_type (str or list): Type of sweep (e.g., 'up', 'down', or ['up', 'down']).
+
+                Returns:
+                tuple: DataFrame containing processed data, filename of the generated plot, full path of the output file.
+                """
+        # Load data
+        df = load_viscosity_stress_data(file_path)
+
+
+        # Generate filename
+        fig_name = os.path.splitext(os.path.basename(file_path))[0]
+        if isinstance(sweep_type, list):
+            fig_name += "-BOTH.png"
+        else:
+            fig_name += f"-{sweep_type}.png"
+
+        # Full path for output file
+        full_output_path = os.path.join(self.output_directory, fig_name)
+
+        # Plot data
+        plot_diff_viscosity_data(
+            df=df,
+            fig_name=fig_name,
+            export_path=self.output_directory,
+            sweep_types=sweep_type
+        )
+
+        return df, fig_name, full_output_path
 
     def process_thixotropy_single(self, file_path):
         """
